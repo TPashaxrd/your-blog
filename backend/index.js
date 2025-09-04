e6cb69a4378ef040114eaf70c1072a19b2eaaf4c
TPashaxrd <tpashaaonline@gmail.com>
2025-09-04 13:47:58 +0300

    fixed all

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const SubsRoutes = require("./routes/subs")
 const app = express()
 
 db()
+
 app.use(cors())
 app.use(express.json())
 app.use('/api/post', limiter, PostRoutes)
@@ -21,4 +22,4 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running at: ${PORT}`)
-})
+}) 
--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit")
 const limiter = rateLimit({
     windowMs: 60 * 1000,
     max: 50,
-    message: "Too many request from this IP, try later."
+    message: "Too many request from thıs IP, Try later...."
 })
 
 module.exports = { limiter }
